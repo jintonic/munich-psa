b0440350a25473e5751378c67b912bf6e6c3edb1
vauth <vauth@mppmu.mpg.de>
2009-07-01 15:26:38 +0000

    Training the network seems to work now for most learning methods, output of the results doesn't. Annika
    git-svn-id: file:///home/jingliu/home/jing.liu/svn/PSA@592 a066fb6a-a811-11dc-9a5e-f027034fa78f

--- compare/network.C
+++ compare/network.C
@@ -1,5 +1,5 @@
 // Build and train the NN for the PSA
-// Use like .x network ("trainingfile.root", 10) (for 10 training iterations)
+// Use like .x network ("trainingfile.root", 100) (for 100 training iterations)
 
 #include <TString.h>
 
@@ -59,67 +59,86 @@ void network(const char* filename, Int_t ntrain){
   TMultiLayerPerceptron *networkPSA = new TMultiLayerPerceptron(layoutChar, "Weight", trainingTree,"Entry$%2","(Entry$+1)%2");
                                                                  // Use half of the events as training and half as test dataset
 
-  //TEST: to check if the kind of network layout desciption and tree work
-  // TMultiLayerPerceptron *networkPSA = new TMultiLayerPerceptron("wfBin_001, wfBin_002, wfBin_003, wfBin_004, wfBin_005:10:type", "Weight", trainingTree, "Entry$%2","(Entry$+1)%2");   cout << " (Using the test network with only 5 inputs) "; 
-
-  //a larger test network
-   Int_t testnum = 50; Int_t testfrom = 111;  TString *testLayout = new TString();   
-   for (Int_t b = testfrom; b < testfrom + testnum ; b++) {    testLayout->Append(branchName[b]);     
-     if (b<testfrom + testnum -1 ) testLayout->Append(", ");   else testLayout->Append(":50:type");     }      const char* testChar = testLayout->Data(); 
-   //   TMultiLayerPerceptron *networkPSA = new TMultiLayerPerceptron(testChar, "Weight", trainingTree,"Entry$%2","(Entry$+1)%2");  
-   // cout << " (Using test network, bins from " << testfrom-1 << " to " << testfrom + testnum -1 << ") ";
+  //a smaller test network
+  Int_t testnum = 50; Int_t testfrom = 111;  TString *testLayout = new TString();   
+  for (Int_t b = testfrom; b < testfrom + testnum ; b++) {    testLayout->Append(branchName[b]);     
+    if (b<testfrom + testnum -1 ) testLayout->Append(", ");   else testLayout->Append(":50:type");     }      const char* testChar = testLayout->Data(); 
+  //TMultiLayerPerceptron *networkPSA = new TMultiLayerPerceptron(testChar, "Weight", trainingTree,"Entry$%2","(Entry$+1)%2");  
+  //cout << " (Using test network, bins from " << testfrom-1 << " to " << testfrom + testnum -1 << ") ";
 
   cout << "Done." << endl;
 
   const char* method;
   // set learning method 
-  //  networkPSA ->  SetLearningMethod(TMultiLayerPerceptron::kStochastic);  method = " kStochstic";              //works sometimes (?), extremly slow
-  networkPSA ->  SetLearningMethod(TMultiLayerPerceptron::kBatch);   method = " kBatch";                      //seems to work
+  //  networkPSA ->  SetLearningMethod(TMultiLayerPerceptron::kStochastic);  method = " kStochstic";              //works sometimes (?), very slow
+  //  networkPSA ->  SetLearningMethod(TMultiLayerPerceptron::kBatch);   method = " kBatch";                      //seems to work
   //  networkPSA ->  SetLearningMethod(TMultiLayerPerceptron::kSteepestDescent);   method = " kDescent";          //seems to work
-  //  networkPSA ->  SetLearningMethod(TMultiLayerPerceptron::kRibierePolak); method = " RibierePolak";               //seems to work
-  //  networkPSA ->  SetLearningMethod(TMultiLayerPerceptron::kFletcherReeves);  method = " kFletcherReeves";     //seems to work
-  //  networkPSA ->  SetLearningMethod(TMultiLayerPerceptron::kBFGS); method = " kBFGS";                          //does not work 
+  //  networkPSA ->  SetLearningMethod(TMultiLayerPerceptron::kRibierePolak); method = " kRibierePolak";           //seems to work
+    networkPSA ->  SetLearningMethod(TMultiLayerPerceptron::kFletcherReeves);  method = " kFletcherReeves";     //seems to work, kind of slow
+  //  networkPSA ->  SetLearningMethod(TMultiLayerPerceptron::kBFGS); method = " kBFGS";                          //does NOT work (only sometimes..)
   cout << endl << "--- Using learning method" << method << ". ---" << endl << endl;
-  networkPSA->Train(ntrain, "text,graph,update=1"); //full output every epoch
+
+
+  networkPSA->Train(ntrain, "text,graph,update=10"); //full output every 10 epochs
+  networkPSA->DumpWeights("weights.txt"); 
 
 
   // ----- Display the results
-  TCanvas* canvas_NN = new TCanvas("canvas_NN"); 
-   TH1F *bg = new TH1F("bgh", "NN output", 150, -1.0, 2.0);
-   TH1F *sig = new TH1F("sigh", "NN output", 150, -1.0, 2.0);
-   bg->SetDirectory(0);
-   sig->SetDirectory(0);
-   Double_t paramsBG[300];
-   Double_t paramsSig[300];
-   for (i = 0; i < trainingTree->GetEntries(); i++) {
-      trainingTree->GetEntry(i);
-      if (type==0){
-      for (b = 0; b < 300 ; b++){
-	paramsBG[b] = branchName[b];
-      }
-      bg->Fill(networkPSA->Evaluate(0, paramsBG));
-      }
-      else {
-      for (b = 0; b < 300 ; b++){
-	paramsSig[b] = branchName[b];
-      }
-      sig->Fill(networkPSA->Evaluate(0,paramsSig));
-      }
-   }
-   bg->SetLineColor(kBlue);
-   bg->SetFillStyle(3008);   bg->SetFillColor(kBlue);
-   sig->SetLineColor(kRed);
-   sig->SetFillStyle(3003); sig->SetFillColor(kRed);
-   bg->SetStats(0);
-   sig->SetStats(0);
-   bg->Draw();
-   sig->Draw("same");
-   TLegend *legend = new TLegend(.75, .80, .95, .95);
-   legend->AddEntry(bg, "Background");
-   legend->AddEntry(sig, "Signal");
-   legend->Draw();
-   mlpa_canvas->cd(0);
 
+  // try the direct way
+  networkPSA->DrawResult(0,"test");  
+
+
+  // Using Evaluate, get the second entries from tree (should give the test dataset) 
+  Double_t params[300];
+  TCanvas* canvas_NN = new TCanvas("canvas_NN","Histo for ->Evaluate", 150, 150, 800, 600); 
+  TH1D *hist_NN = new TH1D("hist_NN","NN output", 200, 0.0, 2.);
+  Double_t NNoutput;  
+  // loop over events 
+  for (Int_t ientries = 0; ientries < nentries/2; ientries++)
+    {
+      trainingTree -> GetEntry(2*ientries+1); 
+      // evaluate NN 
+      for (b = 0; b < 300 ; b++){
+	params[b] = wfBin[b];
+	if(b%100==50 && ientries%250==0) cout << "entry " << 2*ientries+1 << ": params[" << b<< "] = " << params[b] << endl;
+      }
+      NNoutput = networkPSA -> Evaluate(0, params); 
+      if (ientries%250==0) cout << "NNOutput from Evalutate: "<<NNoutput << " Network Result(0): " << networkPSA->Result(ientries,0) <<  endl;
+      hist_NN -> Fill(NNoutput);
+    }
+  canvas_NN -> cd(); 
+  hist_NN -> SetStats(kFALSE); 
+  hist_NN -> Draw(); 
+  
+
+  // other try to display the results
+  TCanvas* canvas_tree = new TCanvas("canvas_tree","Read from tree, evaluate", 300, 300, 800, 600); 
+  canvas_tree->cd();
+  TH1D *bg = new TH1D("bg","NN output", 200, 0.0, 2.0);
+  TH1D *sig = new TH1D("sig","NN output", 200, 0.0, 2.0);
+  for (i = 0; i < trainingTree->GetEntries(); i++) {
+    trainingTree->GetEntry(i);
+    if (type==0){
+      for (b = 0; b < 300 ; b++){
+   	params[b] = wfBin[b];
+      }
+      bg->Fill(networkPSA->Evaluate(0, params));
+    }
+    else {
+      for (b = 0; b < 300 ; b++){
+   	params[b] = wfBin[b];
+      }
+         sig->Fill(networkPSA->Evaluate(0,params));
+    }
+  }  
+  bg->SetLineColor(kBlue);  bg->SetFillStyle(3008);   bg->SetFillColor(kBlue);
+  sig->SetLineColor(kRed);  sig->SetFillStyle(3003); sig->SetFillColor(kRed);
+  bg->SetStats(0);    bg->Draw();
+  sig->SetStats(0);   sig->Draw("same");
+  TLegend *legend = new TLegend(.75, .80, .95, .95);
+  legend->AddEntry(bg, "Background");    legend->AddEntry(sig, "Signal");
+  legend->Draw();
 
 
   delete trainingFile;
